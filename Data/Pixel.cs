ac5992928cb44f177e987b559dd20c63bc3e2d3b
Samat Kurmanov <igvog88@gmail.com>
2020-08-17 01:46:53 +0600

    Test

---
+++ Data/Pixel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyPhotoshop
+{
+    public class Pixel
+    {
+        public double Check(double value)
+        {
+            if (value < 0 || value > 1) throw new ArgumentException();
+            return value;
+        }
+        double r;
+        
+        public double R {
+            get { return r; }
+            set {
+                r = Check(value);
+            }
+        }
+        
+        double g;
+        public double G
+        {
+            get { return g; }
+            set
+            {
+                g = Check(value);
+            }
+        }
+        double b;
+
+        public double B
+        {
+            get { return b; }
+            set
+            {
+                b = Check(value);
+            }
+        }
+
+    }
+}
